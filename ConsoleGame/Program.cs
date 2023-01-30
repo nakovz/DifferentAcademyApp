d68f7da337d1b42d7f552d01e3b67606b4fac26b
ljupkaFSAcademy <ljupkailieska@gmail.com>
2023-01-30 21:53:52 +0100

    Make some comment in Program.cs

--- ConsoleGame/Program.cs
+++ ConsoleGame/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleGame {
             //var myStore = new Store { Name = "Different Accademy" };
             //myStore.WellcomeScreen();
 
+            //Only a little comment from Ljupka Ilieska. Have a good day and night! 
+
             // Test with implementing of an interface
             List<IItemsForSale> menuList = MainMenu.Init();
             MainMenu.MenuWithDetailPage("Different Accademy", "G", menuList.ToArray());
