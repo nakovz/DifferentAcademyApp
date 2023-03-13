8ade8b7d4845d069e3f1fda8aa5716b916128590
ljupkaFSAcademy <ljupkailieska@gmail.com>
2023-03-13 20:53:04 +0100

    Comment *.bak in gitignore file

--- DifferentAcademyeMarket/DAeMarketModel.Designer.cs
+++ DifferentAcademyeMarket/DAeMarketModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Zoran\Source\Repos\HomeWork03\DifferentAcademyeMarket\DAeMarketModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Lenovo\source\repos\ZN_github_repos\DifferentAcademyApp\DifferentAcademyeMarket\DAeMarketModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
